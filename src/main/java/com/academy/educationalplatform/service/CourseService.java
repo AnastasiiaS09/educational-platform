2da3b50dfb71583f4ed724ad46ad58d0da5a00ed
darinakrutenko-eng <darinakrutenko@ukr.net>
2026-07-01 16:35:31 +0300

    commit

--- src/main/java/com/academy/educationalplatform/service/CourseService.java
+++ src/main/java/com/academy/educationalplatform/service/CourseService.java
@@ -1,28 +1,32 @@
 package com.academy.educationalplatform.service;
 
 import com.academy.educationalplatform.entity.Course;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
 
-import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CourseService {
     public Course create(String name, String description, int lectureNumber) {
+        Course course = new Course();
+        return course;
     }
 
     public Course findById(Long id) {
+        Course course = new Course();
+        return course;
     }
 
     public Course update(Long id,String courseName, String description, int lectureNumber) {
+        Course course = new Course();
+        return course;
     }
 
     public void delete(Long id) {
     }
 
-    public List<Course> getAll{
+    public List<Course> getAll(){
+        List<Course> course = new ArrayList<>();
+        return course;
 
     }
 
