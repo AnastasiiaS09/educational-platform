9d46fa90723421a28d579bb35f3a6ddc518c51b8
darinakrutenko-eng <darinakrutenko@ukr.net>
2026-07-29 18:07:07 +0300

    commit

--- src/main/java/com/academy/educationalplatform/exception/PlatformErrorCode.java
+++ src/main/java/com/academy/educationalplatform/exception/PlatformErrorCode.java
@@ -3,35 +3,34 @@ package com.academy.educationalplatform.exception;
 import org.springframework.http.HttpStatus;
 
 public enum PlatformErrorCode {
-    USER_EMAIL_EXISTS(HttpStatus.CONFLICT, "User with this email already exists: %s"),
-    USER_PHONE_EXISTS(HttpStatus.CONFLICT, "User with this phone already exists: %s"),
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User not found: %s"),
+    ACCESS_DENIED(HttpStatus.UNAUTHORIZED, "Access was denied"),
+    ANSWER_OPTION_NOT_FOUND(HttpStatus.NOT_FOUND, "AnswerOption with this number not found"),
 
     COURSE_ALREADY_EXISTS(HttpStatus.CONFLICT, "Course with this name already exists: %s"),
     COURSE_NOT_FOUND(HttpStatus.NOT_FOUND, "Course not found: %s"),
 
-    TEST_ALREADY_EXISTS(HttpStatus.CONFLICT, "Test with this name already exists: %s"),
-    TEST_NOT_FOUND(HttpStatus.NOT_FOUND, "Test not found: %s"),
+    ENGLISH_QUESTION_ALREADY_EXISTS(HttpStatus.CONFLICT, "Question in English test with this number already exists: %s"),
+    ENGLISH_QUESTION_NOT_FOUND(HttpStatus.NOT_FOUND, "Question in English test with this number not found: %s"),
 
-    MODULE_ALREADY_EXISTS(HttpStatus.CONFLICT, "Module with this name already exists: %s"),
-    MODULE_NOT_FOUND(HttpStatus.NOT_FOUND, "Module not found: %s"),
+    FILE_UPLOAD_FAILED(HttpStatus.NOT_FOUND, "FILE_UPLOAD_FAILED"),
+    INVALID_CREDENTIALS(HttpStatus.UNAUTHORIZED, "Invalid email or password"),
+
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "Invalid token"),
 
     LESSON_NOT_FOUND(HttpStatus.NOT_FOUND, "Lesson not found"),
 
     LIKE_NOT_FOUND(HttpStatus.NOT_FOUND, "Like not found"),
+    MODULE_ALREADY_EXISTS(HttpStatus.CONFLICT, "Module with this name already exists: %s"),
+    MODULE_NOT_FOUND(HttpStatus.NOT_FOUND, "Module not found: %s"),
 
-    ENGLISH_QUESTION_ALREADY_EXISTS(HttpStatus.CONFLICT, "Question in English test with this number already exists: %s"),
-    ENGLISH_QUESTION_NOT_FOUND(HttpStatus.NOT_FOUND, "Question in English test with this number not found: %s"),
-    ANSWER_OPTION_NOT_FOUND(HttpStatus.NOT_FOUND, "AnswerOption with this number not found"),
-
-    ANSWER_OPTION_NOT_FOUND(HttpStatus.NOT_FOUND, "Answer option not found"),
-
-    FILE_UPLOAD_FAILED(HttpStatus.NOT_FOUND, "FILE_UPLOAD_FAILED"),
     POSTER_NOT_FOUND(HttpStatus.NOT_FOUND, "Poster not found"),
 
-    INVALID_CREDENTIALS(HttpStatus.UNAUTHORIZED, "Invalid email or password"),
-    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "Invalid token"),
-    ACCESS_DENIED(HttpStatus.UNAUTHORIZED, "Access was denied");
+    TEST_ALREADY_EXISTS(HttpStatus.CONFLICT, "Test with this name already exists: %s"),
+    TEST_NOT_FOUND(HttpStatus.NOT_FOUND, "Test not found: %s"),
+
+    USER_EMAIL_EXISTS(HttpStatus.CONFLICT, "User with this email already exists: %s"),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User not found: %s"),
+    USER_PHONE_EXISTS(HttpStatus.CONFLICT, "User with this phone already exists: %s");
 
 
     private final HttpStatus status;
